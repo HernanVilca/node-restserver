ef269845e17577888d711a943034fcd48ac4deee
HernanVilca <hernanmasvi@gmail.com>
2019-10-17 23:13:39 -0500

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -4,3 +4,23 @@
 
 
 process.env.PORT = process.env.PORT || 3000;
+
+// =======================================
+//      Entorno
+// =======================================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// =======================================
+//      Base de Datos
+// =======================================
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+
+    urlDB = 'mongodb+srv://torres:7TQaBZzlAC9vnPCR@cluster0-4ixyh.mongodb.net/cafe';
+
+}
+
+process.env.URLDB = urlDB;
